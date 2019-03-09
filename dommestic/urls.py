7aaec8037f0c49748949fdd3bd4502da6280016f
Laura Buchanan <laura.cathleen.buchanan@gmail.com>
2019-03-08 21:27:53 -0600

    update url patterns

--- dommestic/urls.py
+++ dommestic/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('/', redirect('/home/'))
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
